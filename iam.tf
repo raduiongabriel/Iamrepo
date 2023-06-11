dfb7e471641148ec97087c71c43d0b3dde29d4bf
raduiongabriel <ionradugabriel06@gmail.com>
2023-06-11 04:58:17 -0400

    ok

--- iam.tf
+++ iam.tf
@@ -1,27 +1,28 @@
-# Fișier: iam.tf
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.68.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
       version = "4.68.0"
     }
-  
-   random = {
-      source = "hashicorp/random"
+
+    random = {
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
+  }
 }
-}
+
 #############################VARIABILE #################################
 variable "project_id" {
   description = "ID-ul proiectului"
   type        = string
   default     = "terraform-386709"
 }
+
 variable "credentials_file" {
   description = "Calea către fișierul de credențiale"
   type        = string
@@ -45,45 +46,40 @@ variable "service_account_email" {
   type        = string
   default     = "null"
 }
-
 #############################VARIABILE #################################
 
-provider "google-beta" {
+provider "google" {
   project     = var.project_id
-  credentials = var.credentials_file
+  credentials = file(var.credentials_file)
 }
-
 
 
 # Definirea politicii IAM pentru un grup
-data "google_iam_policy" "group_policy" {
-    provider = google-beta
-  binding {
-    role    = "roles/editor"
-    members = [
-      "group:${var.group_email}",
-    ]
-  }
+resource "google_project_iam_binding" "group_policy" {
+  project = var.project_id
+  role    = "roles/editor"
+
+  members = [
+    "group:${var.group_email}",
+  ]
 }
 
 # Definirea politicii IAM pentru un utilizator
-data "google_iam_policy" "user_policy" {
-    provider = google-beta
-  binding {
-    role    = "roles/compute.instanceAdmin"
-    members = [
-      "user:${var.user_email}",
-    ]
-  }
+resource "google_project_iam_binding" "user_policy" {
+  project = var.project_id
+  role    = "roles/compute.instanceAdmin"
+
+  members = [
+    "user:${var.user_email}",
+  ]
 }
 
 # Definirea politicii IAM pentru un serviciu
-data "google_iam_policy" "service_policy" {
-    provider = google-beta
-  binding {
-    role    = "roles/storage.objectAdmin"
-    members = [
-      "serviceAccount:${var.service_account_email}",
-    ]
-  }
+resource "google_project_iam_binding" "service_policy" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:${var.service_account_email}",
+  ]
 }
